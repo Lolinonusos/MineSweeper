c28cca46b2fe0a7a8a9c3fb42ecc2b1f82b7b46b
Linus Nordbakken Nagy <linus.nordbakken@gmail.com>
2021-11-19 21:15:43 +0100

    Add project files.

---
+++ MineSweeper/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <conio.h>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <ctime>
+
+// Game functions
+void drawBoard();
+
+// Global
+void clearCin();
+
+int main() {
+
+}
+
+// Draw the board
+void drawBoard() {
+	// change i and j variables to something functional
+	for (int i = 0; i < 1; i++) {
+		for (int j = 0; j < 1; j++) {
+
+		}
+	}
+}
+
+void clearCin() {
+	std::cin.clear();
+	std::cin.ignore(32767, '\n');
+}
